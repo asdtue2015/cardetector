a21a3366377915abf869a94e8eb75e1d3c153c2e
Vincent L.M. Mazoyer <v.l.m.mazoyer@tue.nl>
2016-02-11 15:02:21 +0100

    doxygen script: improved check for required packages

--- docgen.sh
+++ docgen.sh
@@ -1,4 +1,6 @@
-command -v doxygen >/dev/null 2>&1 || { echo >&2 "Utility doxygen is not installed.  Aborting."; exit 1; }
+command -v doxygen >/dev/null 2>&1 || { echo >&2 "Utility doxygen is not installed."; echo >&2 "Install it using: sudo apt-get install doxygen"; exit 1; }
+dpkg -s graphviz >/dev/null 2>&1 || { echo >&2 "Utility dot (graphviz) is not installed."; echo >&2 "Install it using: sudo apt-get install graphviz"; exit 1; }
+dpkg -s doxygen-latex >/dev/null 2>&1 || { echo >&2 "Utility latex is not installed."; echo >&2 "Install it using: sudo apt-get install doxygen-latex"; exit 1; }
 rm -r -f /tmp/doxydoc
 mkdir /tmp/doxydoc
 doxygen .Doxyfile 2>&1 || { echo >&2 "Error while running doxygen.  Aborting."; exit 1; }
