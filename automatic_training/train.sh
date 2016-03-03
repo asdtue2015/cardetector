c363ce7333b3b875f5d38d79f449ed2271e67198
Vincent L.M. Mazoyer <v.l.m.mazoyer@tue.nl>
2016-03-03 11:06:49 +0100

    using directory and filename emulation

--- automatic_training/train.sh
+++ automatic_training/train.sh
@@ -1,40 +1,57 @@
 #!/bin/bash
 STEPS=3
 BATCH_SIZE=20
-IMAGE_DIR="$HOME/Projects/TUE_Multiclass_Detector/data/image_2"
-DETECTOR_DIR="$HOME/asd2015dev/autotrain/src/detector"
-LOCAL_DIR=$(pwd)
+MASTER_DIR="$HOME/Projects/TUE_Multiclass_Detector"
+LOCAL_DIR=$(pwd)/..
+
+# matlab path
+MATLAB=~/MATLAB/R2015b/bin/matlab
+
+# recreate the structure
+(cd $LOCAL_DIR;\
+ rm -r data;\
+ mkdir data;\
+ cd data;\
+ mkdir hog_2;\
+ mkdir image_2;\
+ mkdir label_2;\
+ mkdir nclabel_2;
+ cp $MASTER_DIR/data/negative_features.mat ./negative_features.mat
+ ln $MASTER_DIR/data/positive_features.mat ./positive_features.mat
+ cp $MASTER_DIR/data/carDetector56x48.yml ./carDetector56x48.yml
+ )
 
 # compile the detector
-(cd "$DETECTOR_DIR"; )
+(cd "$LOCAL_DIR/src/detector"; rm CMakeCache.txt; rm -r CMakeFiles; rm detector; cmake .; make)
 
 for i in `seq 0 $STEPS`;
-do	# every step
+do	# every stepl
 
 	# backup the classifier
+	(cp ../data/carDetector56x48.yml ./carDetector56x48.yml.backup$i)
 
 	# create hard links to the files
 	k=0 # files are renamed starting at 0 every time because the matlab script needs this...
 	for j in `seq $[$i*$BATCH_SIZE] $[$[$i+1]*$BATCH_SIZE-1]`;
 	do
-		ln $IMAGE_DIR/$(printf "%06d" $j).png $(printf "%06d" $k).png
+		ln $MASTER_DIR/data/image_2/$(printf "%06d" $j).png $LOCAL_DIR/data/image_2/$(printf "%06d" $k).png
+		ln $MASTER_DIR/data/label_2/$(printf "%06d" $j).txt $LOCAL_DIR/data/label_2/$(printf "%06d" $k).txt
+		ln $MASTER_DIR/data/hog_2/$(printf "%06d" $j).yml $LOCAL_DIR/data/hog_2/$(printf "%06d" $k).yml
 		k=$[$k+1]
 	done
 
 	# run the detector
-	(cd "$DETECTOR_DIR"; ./detector --scale 1.12 --nlevels 13 --gr_threshold 0 --hit_threshold 0 $LOCAL_DIR --write_file)
-	find $DETECTOR_DIR -name "*.txt" -exec mv -i -t $LOCAL_DIR {} +;
+	(cd "$LOCAL_DIR/src/detector"; ./detector --scale 1.12 --nlevels 13 --gr_threshold 0 --hit_threshold 0 $LOCAL_DIR/data/image_2 --write_file)
+	find $LOCAL_DIR/src/detector -name "*.txt" -exec mv -i -t $LOCAL_DIR/data/nclabel_2 {} +;
 
 	# perform hard negative mining
 	# concat to existing negative HOG features
-
-	# backup the negative HOG features
-
 	# train the classifier
+	(cd $LOCAL_DIR/matlab/automatic; $MATLAB -nodesktop -nosplash -nojvm < ngmining_training.m)
 
 	# clean images and labels
 	ls $LOCAL_DIR
-	rm *.png
-	rm *.txt
-
+	rm $LOCAL_DIR/data/image_2/*.png
+	rm $LOCAL_DIR/data/label_2/*.txt
+	rm $LOCAL_DIR/data/hog_2/*.yml
 done
--- matlab/automatic/ngmining_training.m
+++ matlab/automatic/ngmining_training.m
@@ -2,8 +2,8 @@
 clear all; close all; clc;
 
 %% cd to correct dir
-ROOT_DIR = '/home/zhengyu/cardetector/';           % Put data folder path HERE !!!!!
-% ROOT_DIR = '/home/shah/Projects/'
+ROOT_DIR = '/home/shah/asd2015dev/autotrain/';           % Put data folder path HERE !!!!!
+
 cd([ROOT_DIR,'matlab/automatic']);
 addpath '../tools'                                      % Add paths for the tools folder  !!!!
 addpath '../samples/negative/'
@@ -18,7 +18,7 @@ nnclabels   = length(dir(fullfile(nclabel_dir, '*.txt')));
 label_dir = fullfile(data_dir,[data_set '/label_' num2str(cam)]);      % read positive labels
 nlabels   = length(dir(fullfile(label_dir, '*.txt')));
 %% image input
-image_dir = fullfile(data_dir,[data_set '/image_' num2str(cam)]);       % image directory  
+image_dir = fullfile(data_dir,[data_set '/image_' num2str(cam)]);       % image directory
 nimages   = length(dir(fullfile(image_dir, '*.png')));
 %% HOG block input
 hog_dir = fullfile(data_dir,[data_set '/hog_' num2str(cam)]);          % HOG directory
@@ -26,13 +26,13 @@ nymls   = length(dir(fullfile(hog_dir, '*.yml')));
 
 %% Positive matrix input for SVM training
 pos_samples_file = matfile(fullfile(data_dir,'/positive_features.mat'));  % Path of original positive matrix
-pos_samples      = pos_samples_file.bigmat; 
+pos_samples      = pos_samples_file.bigmat;
 
 %% Original negative matrix input for SVM training
 exbigmat  = importdata(fullfile(data_dir,'/negative_features.mat'));             % Oringinal negative matrix path
 
 %% Path to put the new classifier
-fid = fopen('../../data/carDetector56x48_test.yml','w');                 % creat the new SVM classifier set output path HERE !!!!!
+fid = fopen('../../data/carDetector56x48.yml','w');                 % creat the new SVM classifier set output path HERE !!!!!
 
 
 %% main loop
@@ -52,12 +52,12 @@ groupn=ceil(total/npg);                    %calculate group number (with 20 imag
 
 
 for grp=1:1:(groupn+1)
-    
+
     count = 0;
-    
+
     ncbigmat = [] ;
-    for lab_idx = (0+(grp-1)*npg):1:min((grp*npg-1),total)   
-        
+    for lab_idx = (0+(grp-1)*npg):1:min((grp*npg-1),total)
+
         %% parse the label files
         objects   = readLabels(label_dir,lab_idx);                   %read car labels
         total_obj = length(objects);                                 % No. of all the cars
@@ -65,27 +65,27 @@ for grp=1:1:(groupn+1)
         lab_idx
         %% get all car rectrangles
         t      = 1;
-        car_pt = [];                                                 % an object to store all cars in one image 
+        car_pt = [];                                                 % an object to store all cars in one image
 
         dct      = 1;
-        dccar_pt = [];  
+        dccar_pt = [];
 
         for a = 1:total_obj
 
             % get the object label type
-            type = objects(a).type;        
+            type = objects(a).type;
 
             % get left, top, right, bottom pixel coordinates for all cars in image
             if strcmp( type, 'Car' )
                 %% save the roi of cars
                 car_pt.(['c',num2str(t)]) = [objects(a).x1,objects(a).y1,objects(a).x2-objects(a).x1+1,objects(a).y2-objects(a).y1+1]; % get x1, y1, width, height
-                t = t + 1;  
+                t = t + 1;
             end
 
             if strcmp( type, 'DontCare' )
                 %% save the roi of cars
                 dccar_pt.(['dc',num2str(dct)]) = [objects(a).x1,objects(a).y1,objects(a).x2-objects(a).x1+1,objects(a).y2-objects(a).y1+1]; % get x1, y1, width, height
-                dct = dct + 1;  
+                dct = dct + 1;
             end
 
         end
@@ -96,7 +96,7 @@ for grp=1:1:(groupn+1)
 
         %% parse the NotCar label files
         ncobjects   = readncLabels(nclabel_dir,lab_idx);        %read labels from NotCar labels
-        total_ncobj = length(ncobjects);                        % No. of all NotCar 
+        total_ncobj = length(ncobjects);                        % No. of all NotCar
 
         %% get all NotCar rectrangles
         nct      = 1;
@@ -104,20 +104,20 @@ for grp=1:1:(groupn+1)
         for a = 1:total_ncobj
 
             % get the object label type
-            type = ncobjects(a).type;        
+            type = ncobjects(a).type;
 
             % get left, top, right, bottom pixel coordinates for all cars in image
             if strcmp( type, 'NotCar' )
                 %% save the NotCar roi
                 nccar_pt.(['c',num2str(nct)]) = [ncobjects(a).x1,ncobjects(a).y1,ncobjects(a).x2-ncobjects(a).x1+1,ncobjects(a).y2-ncobjects(a).y1+1]; % get x1, y1, width, height
-                nct = nct + 1;  
+                nct = nct + 1;
             end
-        end 
+        end
 
     %%  Generate negetive mining examples
         ncstructSize = length(fieldnames(nccar_pt));       % get total No. of NotCar in a label
         for j=1:ncstructSize                               % iterate through every NotCar label
-          if (t>1)                                          % if there is a 'Car' in label and we have read the labels  
+          if (t>1)                                          % if there is a 'Car' in label and we have read the labels
 
 
                structSize = length(fieldnames(car_pt));       % get total No. of 'Cars'in a label
@@ -132,8 +132,8 @@ for grp=1:1:(groupn+1)
                 for b = 1:structSize                         % get ROI from real cars labels
                     x1 = car_pt.(['c',num2str(b)])((1));       % get x1
                     y1 = car_pt.(['c',num2str(b)])((2));       % get y1
-                    x2 = car_pt.(['c',num2str(b)])((3));       % get width 
-                    y2 = car_pt.(['c',num2str(b)])((4));       % get height 
+                    x2 = car_pt.(['c',num2str(b)])((3));       % get width
+                    y2 = car_pt.(['c',num2str(b)])((4));       % get height
 
                     test(b) = rectangle_collision(x1,y1,x2,y2,x,y,w,h); % check for overlaps between NotCar and Car, add the result to a cell to test
                    % test(b) = collision_area_based(x1,y1,x2,y2,x,y,w,h);
@@ -142,15 +142,15 @@ for grp=1:1:(groupn+1)
 
                 if (dct>1)                                            % if there is a DontCare label
                     dcstructSize = length(fieldnames(dccar_pt));       % get total No. of 'DontCare'in a label
-                    dctest = logical(ones(1,dcstructSize));               
+                    dctest = logical(ones(1,dcstructSize));
                      for b = 1:dcstructSize                         % get ROI from DontCare labels
                          x1 = dccar_pt.(['dc',num2str(b)])((1));       % get x1
                          y1 = dccar_pt.(['dc',num2str(b)])((2));       % get y1
-                         x2 = dccar_pt.(['dc',num2str(b)])((3));       % get width 
-                         y2 = dccar_pt.(['dc',num2str(b)])((4));       % get height 
+                         x2 = dccar_pt.(['dc',num2str(b)])((3));       % get width
+                         y2 = dccar_pt.(['dc',num2str(b)])((4));       % get height
 
 
-                          dctest(b) = rectangle_collision(x1,y1,x2,y2,x,y,w,h); % check for overlaps between NotCar and Dontcare, add the result to a cell to test 
+                          dctest(b) = rectangle_collision(x1,y1,x2,y2,x,y,w,h); % check for overlaps between NotCar and Dontcare, add the result to a cell to test
                         %  dctest(b) = collision_area_based(x1,y1,x2,y2,x,y,w,h);
 
                      end
@@ -171,7 +171,7 @@ for grp=1:1:(groupn+1)
 
                 else                                                         % if there is no overlap, record this NotCar label
                    count = count + 1;
-                   
+
                    %% construct a negative feature for this NotCar label
                     [out,window]   = constructHOGFeature( YamlStruct, x, y, w, h, 0 );
                     ncbigmat(count,:)= out(1,:);
@@ -182,9 +182,9 @@ for grp=1:1:(groupn+1)
           else                                          % if there is no car in this image, then all NotCar should be recorded.
             x = nccar_pt.(['c',num2str(j)])((1));       % get x1
             y = nccar_pt.(['c',num2str(j)])((2));       % get y1
-            w = nccar_pt.(['c',num2str(j)])((3));       % get width 
-            h = nccar_pt.(['c',num2str(j)])((4));       % get height   
-            count = count + 1; 
+            w = nccar_pt.(['c',num2str(j)])((3));       % get width
+            h = nccar_pt.(['c',num2str(j)])((4));       % get height
+            count = count + 1;
             %% construct a negative feature
             [out,window]   = constructHOGFeature( YamlStruct, x, y, w, h, 0 );
             ncbigmat(count,:)= out(1,:);
@@ -192,14 +192,14 @@ for grp=1:1:(groupn+1)
           end
 
 
-        end 
+        end
 
 
     end
     %%
     bigmat=[bigmat;ncbigmat];
-    
-    
+
+
     fclose('all');
 end
 %%
@@ -209,10 +209,10 @@ exbigmat=[exbigmat;bigmat];                % Combine NG matrix with original mat
 
 %% %%%%%%%%%%%%
 % SVM %%%%%%%%%
-%%%%%%%%%%%%%%%%%% 
+%%%%%%%%%%%%%%%%%%
 window           = [56 48];
 npos             = size(pos_samples,1);
-neg_samples      = exbigmat;  
+neg_samples      = exbigmat;
 nneg             = size(neg_samples,1);
 ndim             = size(neg_samples,2);
 
@@ -224,10 +224,10 @@ SVMModel = fitcsvm( [pos_samples;neg_samples], labels );
 
 %% write yml the classifiers file
 
-fprintf(fid,'%%YAML:1.0\n');  
+fprintf(fid,'%%YAML:1.0\n');
 fprintf(fid,'width: %i\n',window(1));
 fprintf(fid,'height: %i\n',window(2));
-fprintf(fid,'detector: [ ');   
+fprintf(fid,'detector: [ ');
 for i = 1:1:size(SVMModel.Beta,1)
     fprintf(fid,'%.10d, ',SVMModel.Beta(i));
 end
